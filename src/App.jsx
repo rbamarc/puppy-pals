a5e877f5ee01852d3de205edb2ca0bab41cf61ac
rbamarc <rbamarc929@gmail.com>
2023-07-25 06:24:58 -0700

    adding component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
+import { puppyList } from './data'
 import './App.css'
+import { useState } from 'react'
+
 
 function App() {
   
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <>
       <div>
-        
+        {
+          puppies.map((puppy) => {
+            return <p key={puppy.id}>{puppy.name }</p>
+          })
+        }
       </div>
     </>
   )
